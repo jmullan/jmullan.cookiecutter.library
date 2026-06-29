a1658f9f147af2db771e89cc3c8301e48f05f4b8
Jesse Mullan <jmullan@gmail.com>
2026-06-28 17:54:13 -0700

    Various

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_namespace}}/{{cookiecutter.project_package}}/_jmullan_cli.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_namespace}}/{{cookiecutter.project_package}}/_jmullan_cli.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.13
+"""Rhe main command-line entrypoint."""
 import logging
 
 from jmullan.cmd import cmd
@@ -7,12 +8,13 @@ from jmullan.logging import easy_logging
 logger = logging.getLogger(__name__)
 
 
-class Main(cmd.Main):
-    """Do a thing"""
+class Main(cmd.Main) -> None:
+    """Do a thing."""
     def __init__(self):
         super().__init__()
 
-    def setup(self):
+    def setup(self) -> None:
+        """Do something after parsing args but before main."""
         super().setup()
         if self.args.verbose:
             easy_logging.easy_initialize_logging("DEBUG")
@@ -20,12 +22,14 @@ class Main(cmd.Main):
             easy_logging.easy_initialize_logging()
 
 
-    def main(self):
+    def main(self) -> None:
+        """Do something after getting set up."""
         super().main()
         logger.debug(self.args)
 
 
-def main():
+def main() -> None:
+    """Run the command via the command-line entrypoint."""
     Main().main()
 
 
